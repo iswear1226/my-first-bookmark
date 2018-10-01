b762d280b373068f8b8bb9de1501a4eeccae7377
wm7082wm <wm7082wm@nate.com>
2018-10-01 09:55:51 +0900

    bookmark_first

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 
 from bookmark.views import *
 
+from bookmark.views import BookmarkLV, BookmarkDV
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
