7fb5fe2f473cd57e04e532de6209c7a8f271e161
wm7082wm <wm7082wm@nate.com>
2018-10-01 13:03:14 +0900

    Changed ...

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 # 북마크 앱 모델에서 클래스 임포트
 from bookmark.models import Bookmark
 # 어드민 클래스에서 리스트 출력 필드 항목을 2개 지정
-
+from django.contrib import admin
+# 북마크 앱 모델에서 클래스 임포트
+from bookmark.models import Bookmark
 
 class BookmarkAdmin(admin.ModelAdmin):
     list_display = ('title', 'url')
--- bookmark/views.py
+++ bookmark/views.py
@@ -6,6 +6,10 @@ from django.views.generic import ListView, DetailView
 from bookmark.models import Bookmark
 
 # Create your views here.
+# 클래스형 제네릭 뷰를 사용하기 위한 임포트
+from django.views.generic import ListView,DetailView
+# 모델 클래스 임포트
+from bookmark.models import Bookmark
 
 #--- ListView
 class BookmarkLV(ListView) :
