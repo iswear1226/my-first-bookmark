3fd304fcf23bb7845fba61604656b2c66c307763
wm7082wm <wm7082wm@nate.com>
2018-10-01 09:57:57 +0900

    Changed ...

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 
 from bookmark.views import *
 
-from bookmark.views import BookmarkLV, BookmarkDV
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
