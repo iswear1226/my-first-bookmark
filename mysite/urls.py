bbad505794c9d7068ec44333da8cf2583fc128e2
wm7082wm <wm7082wm@nate.com>
2018-10-01 13:07:57 +0900

    Changed ...

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 from bookmark.views import *
 # 장고 내장 함수 url() 임포트
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 # URL 패턴에 따라 뷰를 호출할 예정(아직 해당 뷰 코드는 작성 전 상태)
